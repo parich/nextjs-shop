e848bb352105ee66d16c0e355e19d3693e54def7
parich <parich.suriya@gmail.com>
2022-07-21 21:57:14 +0700

    k

--- pages/product/[id].js
+++ pages/product/[id].js
@@ -1,5 +1,5 @@
-import Product from '../../../models/Product';
-import db from '../../../utils/db';
+import Product from "../../../models/Product";
+import db from "../../../utils/db";
 
 const handler = async (req, res) => {
   await db.connect();
